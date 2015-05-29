af36accb4a519847836846f4e76a18b9de178a46
vabh <anuvabhdutt@gmail.com>
2015-05-29 15:39:34 +0200

    tick format

--- scripts/read_files.m
+++ scripts/read_files.m
@@ -1,6 +1,7 @@
-% d = dir('../spam_users_timeline/*.txt_epoch');
+% d = dir('*.txt_epoch');
 % nfiles = length(d);
 % data = [];
+% 
 % for k = 1:nfiles
 %    data = [data importdata(d(k).name)];
 %    disp(d(k).name);
@@ -8,6 +9,17 @@
 
 hold on
 for k = 1:nfiles
+    
     plot(data(:,k), zeros(1,300) + k, '.');
 end
+
+
+xt = get(gca, 'XTick');
+xt = datestr(xt./86400 + datenum(1970,1,1))
+set(gca,'XTickLabel',xt(:, 13:20));
+title('Tweet timeline', 'FontSize', 20)
+xlabel({'Time','(in seconds from Unix epoch)'}, 'FontSize',20)
+ylabel('Accounts', 'FontSize', 20);
+grid on
+grid minor
 hold off
